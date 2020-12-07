e02e67ad343a85a1beae3b4a03148caf7da6e644
samgongora <saul.santiago2426@gmail.com>
2020-12-07 01:48:21 -0500

    cambio de logo

--- app/modulos/plantillas/nav_header_sidebar.php
+++ app/modulos/plantillas/nav_header_sidebar.php
@@ -4,8 +4,8 @@
         <div class="nav-header">
             <div class="brand-logo">
                 <a href="index.php">
-                    <b class="logo-abbr"><img src="recursos/images/logo.png" alt=""> </b>
-                    <span class="logo-compact"><img src="recursos/images/logo-compact.png" alt=""></span>
+                    <b class="logo-abbr"><img src="recursos/images/Ae.png" alt=""> </b>
+                    <span class="logo-compact"><img width="110"src="recursos/images/Agenda escolar (2).png" alt=""></span>
                     <span class="brand-title">
                         <img src="recursos/images/logo-text.png" alt="">
                     </span>
